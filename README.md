2c8fc0165c9461e981a56c9fd586e4334010e54d
szkiss04 <zsoltszilardkiss04@gmail.com>
2022-06-22 21:08:22 +0200

    Changes in README, minor fixes in the stylesheet and the script
    Now the calculator stops functioning if it encounters a math error
    (e.g. division by 0).

--- README.md
+++ README.md
@@ -1,5 +1,15 @@
-# odin-calculator
-odin-calculator
+# JavaScript Calculator
 
-This project is part of "The Odin Project", the open-source web development
-curriculum.
+So here is the mandatory calculator project! Nothing fancy (the color scheme is
+especially a nightmare for a designer), but all functions work as expected. It is
+very simple, but I must say I enjoyed making it!
+
+TO-DO's:
+    - The code needs to be cleaned-up.
+    - Keyboard support!
+    - Backspace support!
+    - Maybe an expression parser/evaluator? (Not JavaScript eval()!)
+
+Live demo: https://szkiss.github.io/odin-calculator
+
+This project is part of "The Odin Project" curriculum.
--- script.js
+++ script.js
@@ -15,32 +15,34 @@ const equalsButton = document.querySelector("button[data-equals]");
 
 let isNullAllowed = false;
 let isDecimalPlaced = false;
+let errorFlag = false; // if we run into a math error, only a reset can get us back to life!
 
-let digits = [ "0" ]; // array for storing the digits
+let digits = ["0"]; // array for storing the digits
 
 let accumulator = 0; // variable for the accumulator
 let ans = undefined; // let's use the result of the previous calculation!
 let currentOperator = ""; // variable to store the current operator
 
-function updateScreen( string, screenElem ) {
+function updateScreen(string, screenElem) {
     screenElem.innerText = string;
 }
 
-function reset( ) {
+function reset() {
     isNullAllowed = false;
     isDecimalPlaced = false;
     accumulator = 0;
     currentOperator = "";
-    digits = [ "0" ];
-    updateScreen( accumulator, screenElement );
+    digits = ["0"];
+    errorFlag = false;
+    updateScreen(accumulator, screenElement);
 }
 
-function arrayToNumber( array, isFloat ) {
-    return isFloat ? parseFloat( array.join("") ) : parseInt( array.join("") );
+function arrayToNumber(array, isFloat) {
+    return isFloat ? parseFloat(array.join("")) : parseInt(array.join(""));
 }
 
-function operate( operandA, operandB, operator ) {
-    switch( operator ) {
+function operate(operandA, operandB, operator) {
+    switch (operator) {
         case "+":
             return operandA + operandB;
         case "-":
@@ -48,9 +50,10 @@ function operate( operandA, operandB, operator ) {
         case "*":
             return operandA * operandB;
         case "/":
-            if( operandB != 0 ) {
+            if (operandB != 0) {
                 return operandA / operandB;
             }
+            errorFlag = true;
             return NaN;
             break;
         default:
@@ -60,64 +63,77 @@ function operate( operandA, operandB, operator ) {
 
 // attaching the event listeners to the buttons
 
-clearButton.addEventListener( "click", reset );
+clearButton.addEventListener("click", reset);
 
-numberButtons.forEach( ( element ) => {
+numberButtons.forEach((element) => {
+
     element.addEventListener("click", () => {
+        if (errorFlag) return;
         ans = undefined;
-        digits.push( element.innerText );
-        updateScreen( arrayToNumber( digits, isDecimalPlaced ), screenElement );
-    } )
-} );
+        digits.push(element.innerText);
+        updateScreen(arrayToNumber(digits, isDecimalPlaced), screenElement);
+    })
+});
 
-operatorButtons.forEach( ( element ) => {
-    element.addEventListener( "click", () => {
-        
-        if ( currentOperator == "" ) {
+operatorButtons.forEach((element) => {
+
+    element.addEventListener("click", () => {
+
+        if (errorFlag) return;
+
+        if (currentOperator == "") {
 
             currentOperator = element.innerText;
-            
-            if ( ans != undefined ) {
-                accumulator =  ans;
+
+            if (ans != undefined) {
+                accumulator = ans;
                 ans = undefined;
-            } else { 
-                accumulator = arrayToNumber( digits, isDecimalPlaced );
-                digits.splice( 1, digits.length - 1 );
+            } else {
+                accumulator = arrayToNumber(digits, isDecimalPlaced);
+                digits.splice(1, digits.length - 1);
             }
 
             isDecimalPlaced = false;
             return;
         }
-        
-        accumulator = operate( accumulator, arrayToNumber( digits, isDecimalPlaced ), currentOperator);
-        
-        if ( isNaN(accumulator) ) {
-            updateScreen( "Math error", screenElement );
+
+        accumulator = operate(accumulator, arrayToNumber(digits, isDecimalPlaced), currentOperator);
+
+        if (isNaN(accumulator)) {
+            updateScreen("Math error", screenElement);
             return;
         }
-        
+
         currentOperator = element.innerText;
-        digits.splice( 1, digits.length - 1);
+        digits.splice(1, digits.length - 1);
         isDecimalPlaced = false;
-        updateScreen( Number.isInteger( accumulator ) ? parseInt( accumulator ) : accumulator.toFixed(8), screenElement );
+        updateScreen(Number.isInteger(accumulator) ? parseInt(accumulator) : accumulator.toFixed(8), screenElement);
     })
 });
 
-equalsButton.addEventListener( "click", () => {
-    if( currentOperator != "" ) {
-        accumulator = operate( accumulator, arrayToNumber( digits, isDecimalPlaced ), currentOperator );
-        ans = Number.isInteger( accumulator ) ? parseInt( accumulator ) : accumulator.toFixed(8);
-        updateScreen( ans, screenElement );
+equalsButton.addEventListener("click", () => {
+    if (errorFlag) return;
+    if (currentOperator != "") {
+        accumulator = operate(accumulator, arrayToNumber(digits, isDecimalPlaced), currentOperator);
+
+        if (isNaN(accumulator)) {
+            updateScreen("Math error", screenElement);
+            return;
+        }
+
+        ans = Number.isInteger(accumulator) ? parseInt(accumulator) : accumulator.toFixed(8);
+        updateScreen(ans, screenElement);
         currentOperator = "";
         accumulator = 0;
-        digits.splice( 1, digits.length - 1 );        
+        digits.splice(1, digits.length - 1);
     }
 })
 
-decimalButton.addEventListener( "click", () => {
-     if( isDecimalPlaced ) return;
-     digits.push(".");
-     isDecimalPlaced = true;
+decimalButton.addEventListener("click", () => {
+    if (errorFlag) return;
+    if (isDecimalPlaced) return;
+    digits.push(".");
+    isDecimalPlaced = true;
 });
 
 reset();
